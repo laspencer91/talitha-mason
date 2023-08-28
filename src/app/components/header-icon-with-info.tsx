6074fcebb82f7b61a39461f5837054e76c8cd719
laspencer91 <laspencer@live.com>
2023-08-28 09:45:35 -0500

    Adjusted styling and other minor changes.

---
+++ src/app/components/header-icon-with-info.tsx
@@ -0,0 +1,5 @@
+import {User} from "react-feather";
+
+<div className='flex flex-row gap-2'>
+    <div className='w-7 h-7 p-[6px] rounded-full background-2 flex justify-center items-center'><User color='white' /></div>Talitha Mason
+</div>
